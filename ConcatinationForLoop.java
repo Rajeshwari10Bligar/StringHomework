a58db1bdc4cad5f8db5194c619d6e3c3f8524c58
https://github.com/Rajeshwari10Bligar/WhileLoop.git <rajeshwaribaligar@gmail.com>
2019-01-09 17:46:13 +0000

    HomeWork

---
+++ ConcatinationForLoop.java
@@ -0,0 +1,13 @@
+package loveProgramming;
+
+public class ConcatinationForLoop {
+    public static void main(String[] args){
+    String myString = " ";
+        for(int i=20;i>=1;i--){
+        myString = myString+i;
+    }
+        System.out.println(myString);
+            }
+
+        }
+
